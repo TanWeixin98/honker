fa859631e95503a784b0b68089f3b33f4cb9fdda
Brian <bjlee@cs.stonybrook.edu>
2019-10-19 05:56:41 +0000

    /adduser code cleanup

--- backend/UserAuthAPI/src/userAuth.js
+++ backend/UserAuthAPI/src/userAuth.js
@@ -4,8 +4,9 @@ const db = dbUtils.getDB().collection('users');
 module.exports = {
 
     addUser: (req, res) => {
-        if(req.body.email == null || req.body.displayName == null){
+        if(!req.body.email || !req.body.displayName){
             res.json({ msg: 'An email and display name are required for registration.' });
+            console.log(JSON.stringify(req.body) + ' => Empty fields');
             return;
         }
         isUniqueUser(req.body)
@@ -16,30 +17,29 @@ module.exports = {
                         msg = 'An account has already been created with this email.';
                     else
                         msg =  'This display name is already in use.';
-                    console.log(req.body + ' ' + msg);
+                    console.log(JSON.stringify(req.body) + ' ' + msg);
                     res.json( {status: 'error', error: msg });
                     return;
                 }
-                else{
-                    var verificationKey = Math.floor(100000 + Math.random() * 900000);
-                    var user = {
-                        email: req.body.email,
-                        password: req.body.password,
-                        verificationKey: verificationKey,
-                        displayName: req.body.displayName
-                    };
-                    db.insertOne(user)
-                        .then((acknowledged) => {
-                            if(!acknowledged) {
-                                res.json({ status: 'error', error: 'Failed to add new user. Please try again later.' });
-                                return;
-                            }
-                            console.log('Inserted new user: ' + user);
-                            res.json({ status: 'OK' });
-                        });
-                }
+                var verificationKey = Math.floor(100000 + Math.random() * 900000);
+                var user = {
+                    email: req.body.email,
+                    password: req.body.password,
+                    verificationKey: verificationKey,
+                    displayName: req.body.displayName
+                };
+                db.insertOne(user)
+                    .then((acknowledged) => {
+                        if(!acknowledged) {
+                            res.json({ status: 'error', error: 'Failed to add new user. Please try again later.' });
+                            return;
+                        }
+                        console.log('Inserted new user: ' + JSON.stringify(user));
+                        res.json({ status: 'OK' });
+                    });
             });
     }
+
 }
 
 function isUniqueUser(params){
