d7011f6313524ca1f6530f2c269b56b3929f3838
SBU-bjlee <47370738+SBU-bjlee@users.noreply.github.com>
2019-10-20 15:37:46 -0400

    Basic Entrypoint model with AddUser() as sample function (#5)
    * Basic Entrypoint model with AddUser() as sample function

---
+++ EntryPoint/src/clientMessenger.js
@@ -0,0 +1,44 @@
+var amqp = require('amqplib');
+var EventEmitter = require('events');
+var uuidv4 = require('uuid/v4');
+
+const RPC_QUEUE = 'UserAuth';
+const REPLY_QUEUE = 'amq.rabbitmq.reply-to';
+const RABBITMQ_URL = 'amqp://localhost';
+var _channel;
+
+module.exports = {
+
+    sendRPCMessage :  (message, endpoint) => {
+        return new Promise((resolve) => {
+            const correlationId = uuidv4();
+            // listen for the content emitted on the correlationId event
+            _channel.responseEmitter.once(correlationId, resolve);
+            _channel.sendToQueue(RPC_QUEUE, new Buffer.from(message), 
+                { 
+                    correlationId, 
+                    replyTo: REPLY_QUEUE,
+                    type: endpoint // Use this to specify which endpoint so the service can figure out which function to call
+                })
+        });
+    },
+
+    createClient: () => {
+        amqp.connect(RABBITMQ_URL)
+            .then((conn) => conn.createChannel())
+            .then((channel) => {
+                // create an event emitter where rpc responses will be published by correlationId
+                channel.responseEmitter = new EventEmitter();
+                channel.responseEmitter.setMaxListeners(0);
+                channel.consume(REPLY_QUEUE,
+                    (msg) => {
+                        channel.responseEmitter.emit(msg.properties.correlationId, JSON.parse(msg.content));
+                        console.log('Received', msg.content.toString());
+                    },
+                    {noAck: true});
+
+                _channel =  channel;
+            });
+    }
+
+}
