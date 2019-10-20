4228399df74954ad7657860fcd97a0a41bc9a297
weixin tan <weixin.tan@stonybrook.edu>
2019-10-20 04:44:43 +0000

    * search by id * search by ts, sorted and limited * mongo module for search and add * more logging

--- tweet_management/tweet_service.js
+++ tweet_management/tweet_service.js
@@ -1,18 +1,27 @@
 const amqp = require('amqplib/callback_api');
 
 const Logger = require('./utils/logger.js');
-//const mongodb = require('../utils/mongodb.js')
+const mongodb = require('../utils/mongodb.js')
 
 const amqp_url = "amqp://localhost"
-const mongo_url = ""
+const mongo_url = "mongodb://192.168.122.12:27017/tweet"
 
 var logger = new Logger("tweet_micro.log");
+
+mongodb.start_connection(mongo_url, "tweet", function(db_err){
+  if(db_err){
+    logger.error("Failed to connect mongodb.", db_err);
+    return;
+  }
+  logger.info("Connected to mongodb");
+});
 
 amqp.connect(amqp_url, function(connection_err, connection){
   if(connection_err){
     logger.error("Failed to connect rabbitmq.", connection_err);
     return;
   }
+
   logger.info("Connected to rabbitmq");
 
   connection.createChannel(function(channel_err, channel){
@@ -34,7 +43,15 @@ amqp.connect(amqp_url, function(connection_err, connection){
       }
       channel.bindQueue(add_queue.queue, exchange ,'add_tweet');
       channel.consume(add_queue.queue,function(msg){
-        console.log(msg);
+        var payload_str = msg.content.toString();
+        var payload = JSON.parse(payload_str);
+        add_item(payload, function(err){
+          if(err){
+            logger.error("Failed to add: " + payload_str ,err);
+            return;
+          }
+          logger.info("Item added: " + payload_str);
+        }) 
       }, {noAck: true});
     });
 
@@ -47,22 +64,44 @@ amqp.connect(amqp_url, function(connection_err, connection){
       channel.bindQueue(search_queue.queue, exchange, 'search_tweet');
       channel.consume(search_queue.queue, function(msg){
         //result is the return item
-        channel.sendToQueue('tweet_rpc', Buffer.from(result.toString()),
-                        {replyTo: search_queue.queue});
+        var payload_str = msg.content.toString();
+        var payload = JSON.parse(payload_str);
+        search_item(payload.id, {"ts" : payload.ts, "limit" : payload.limit}, function(err, result){
+          if(err){
+            logger.error("Failed to search for: " + payload_str, err);
+            return;
+          }
+          logger.info("Item search: " + payload_str 
+                      + " RESULT:" + result.toString());
+          channel.sendToQueue('tweet_rpc', Buffer.from(result.toString()), {replyTo: search_queue.queue});
+        });
       },{noAck: true});
     });
-
   });
 });
 
-function add_item(user_name, content, childType){
-  //mongodb.add();
+function add_item(payload, callback){
+  mongodb.add("tweet", payload, function(err){
+    if(err) return callback(err);
+    return callback(null);
+  });
 }
 
+//TODO del tweet
 function del_item(){}
 
-function search_item(id, options){
-  if(id === undefined){
-    
+function search_item(id, options, callback){
+  if(id === ""){
+    var query = {'ts': {$lte: options.ts}};
+    mongodb.search("tweet", query, options.limit, {'ts': -1},function(err, result){
+      if(err) return callback(err, null);
+      return callback(null, result);
+    });
+  }else{
+    var query = {'id': id};
+    mongodb.search("tweet", query, 1 , {}, function(err, result){
+      if(err) return callback(err, null);
+      return callback(null, result);
+    });
   }
 }
