70a05856e81b3a32242e79b36658e07735f481fe
Brian <bjlee@cs.stonybrook.edu>
2019-10-20 03:52:22 +0000

    Server react app as static file?

--- backend/UserAuthAPI/src/index.js
+++ backend/UserAuthAPI/src/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var path = require('path');
 
 var app = express();
 app.listen(8080);
+app.use('/', express.static(path.join(process.cwd(), '/build')));
+//Use this in master
+//console.log(path.resolve(__dirname, '../../..'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
