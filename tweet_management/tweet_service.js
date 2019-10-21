537f5360046319999e64c2d3e6bed5e3a45406cd
weixin tan <weixin.tan@stonybrook.edu>
2019-10-21 15:00:27 +0000

    corrected response code

--- tweet_management/tweet_service.js
+++ tweet_management/tweet_service.js
@@ -53,7 +53,7 @@ amqp.connect(amqp_url, function(connection_err, connection){
             res = {"status" : "error", "error":"failed to add tweet."};
           }else{
             logger.info("Item added: " + payload_str);
-            res = {"status" : "ok", "id" : payload.id};
+            res = {"status" : "OK", "id" : payload.id};
           }
          
           res = JSON.stringify(res);
@@ -83,7 +83,7 @@ amqp.connect(amqp_url, function(connection_err, connection){
           }else{
             logger.info("Item search: " + payload_str 
                         + " RESULT:" + result);
-            res = {"status" : "ok"}
+            res = {"status" : "OK"}
             if(Array.isArray(result)) res['items'] = result;
             else res['item'] = result;
           }
