25e11b972160961c2fccea98d7d9fd61c8054ffb
weixin tan <weixin.tan@stonybrook.edu>
2019-10-22 21:50:20 +0000

    search item individual

--- temp/show.js
+++ temp/show.js
@@ -1,4 +1,15 @@
 $(document).ready(function (){
+        $('#item').click(function(){
+          var id = $('#item_id').val();
+          $.ajax({
+            type:'GET',
+            url: '/item/'+id,
+          }).done(function(res){
+            $('#list').empty();
+            $('#list').append('<li>'+"username:"+res.item.username+"  content:    "+res.item.content+'</li>');
+          })
+        
+        });
         $('#search').click(function(){
           var limit = $('#limit').val();
           var ts = $('#ts').val;
@@ -8,6 +19,7 @@ $(document).ready(function (){
             contentType: 'application/json',
             data: {"limit": limit, ts:"ts"},
           }).done(function(res){
+            $('#list').empty();
             $.each(res.items, function(i, item){
               $('#list').append('<li>'+"username:"+item.username+"  content:"+item.content+'</li>');
             
