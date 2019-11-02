e1406b1feaf220cd1934bb01d6a5b497eeb4527f
weixin tan <weixin.tan@stonybrook.edu>
2019-11-02 17:09:57 +0000

    response return list of id

--- tweet_management/tweet_service.js
+++ tweet_management/tweet_service.js
@@ -89,7 +89,13 @@ amqp.connect(amqp_url, function(connection_err, connection){
             logger.info("Item search: " + payload_str 
                         + " RESULT:" + result);
             res = {"status" : "OK"}
-            if(Array.isArray(result)) res['items'] = result;
+            if(payload.username !== undefined){
+              res['items'] = [];
+              result.forEach(function(element) {
+                res['items'].push(element.id);
+              });
+            }
+            else if(Array.isArray(result)) res['items'] = result;
             else res['item'] = result;
           }
           
