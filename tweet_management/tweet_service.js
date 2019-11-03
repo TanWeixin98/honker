d2c28fbd316de2fbfe461397c35db903e82a8475
Weixin Tan <weixin.tan@stonybrook.edu>
2019-11-03 14:35:14 -0500

    Search filters and delete (#13)
    * add delete and update search entry point
    ** search filter by followers if indicated in msg
    * search using keyword
    * searchh using username

--- tweet_management/tweet_service.js
+++ tweet_management/tweet_service.js
@@ -2,10 +2,12 @@ const amqp = require('amqplib/callback_api');
 
 const Logger = require('./utils/logger.js');
 const mongodb = require('./utils/mongodb.js');
+const utils = require('./tweet_utils.js');
 
 const mongo_ip = "localhost";
 const amqp_url = "amqp://localhost";
 const mongo_url = "mongodb://" + mongo_ip + "/tweet";
+
 
 var logger = new Logger("tweet_micro.log");
 
@@ -15,6 +17,12 @@ mongodb.start_connection(mongo_url, "tweet", function(db_err){
     return;
   }
   logger.info("Connected to mongodb");
+  mongodb.index('tweet', {username : "text", content: "text"}, function(index_err){
+    if(index_err){
+      logger.error("Failed to index username and content for tweets");
+      return;
+    }
+  });
 });
 
 amqp.connect(amqp_url, function(connection_err, connection){
@@ -63,6 +71,35 @@ amqp.connect(amqp_url, function(connection_err, connection){
       });
     });
 
+    //remove 
+    channel.assertQueue('delete_item', {exclusive: true, durable: false}, function(remove_err, remove_queue){
+      if(remove_err){
+        logger.error("Assert search queue failed. ", remove_err);
+        return;
+      }
+      logger.info("Assert remove item queue success");
+
+      channel.bindQueue(remove_queue.queue, exchange, 'remove_tweet');
+      channel.consume(remove_queue.queue, function(msg){
+        var payload_str = msg.content.toString();
+        var payload = JSON.parse(payload_str);
+        del_item(payload, function(err){
+          var res = {};
+          if(err){
+            logger.error("Failed to remove" + payload_str, err );
+            res = {"status": "error", "error":"failed to remove tweet"};
+          }else{
+            logger.info("Item removed: " + payload_str);
+            res = {"status" :"OK", "id" : payload.id};
+          }
+          
+          res = JSON.stringify(res);
+          channel.sendToQueue(msg.properties.replyTo, Buffer.from(res), {correlationId: msg.properties.correlationId});
+          channel.ack(msg);
+        });
+      });
+    });
+
     //search
     channel.assertQueue('search_item', {exclusive: true, durable: false}, function(search_err, search_queue){
       if(search_err){
@@ -76,24 +113,36 @@ amqp.connect(amqp_url, function(connection_err, connection){
         var payload_str = msg.content.toString();
         var payload = JSON.parse(payload_str);
 
-        search_item(payload.id, {"timestamp" : payload.timestamp, "username": payload.username,"limit" : payload.limit, "projections": {}}, function(err, result){
-          var res = {};
-          if(err){
-            logger.error("Failed to search for: " + payload_str, err);
-            res = {"status":"error", "error":"failed to search tweet"};
-          }else{
-            logger.info("Item search: " + payload_str 
-                        + " RESULT:" + result);
-            res = {"status" : "OK"}
-            if(Array.isArray(result)) res['items'] = result;
-            else res['item'] = result;
-          }
+        search_item(payload.id, {"timestamp" : payload.timestamp, 
+                                 "username": payload.username,
+                                 "limit" : payload.limit, 
+                                 "projections": {},
+                                 "query" : payload.query}, 
+          function(err, result){
+            var res = {};
+            if(err){
+              logger.error("Failed to search for: " + payload_str, err);
+              res = {"status":"error", "error":"failed to search tweet"};
+            }else{
+              logger.info("Item search: " + payload_str 
+                          + " RESULT:" + result);
+              res = {"status" : "OK"}
+              if(Array.isArray(result)){
+                res['items'] = result;
+                console.log(result);
+                console.log(payload.follower_list);
+                if(payload.following){
+                  res['items'] = utils.filter_tweets(result, payload.follower_list);  
+                }
+              }
+              else res['item'] = result;
+            }
           
-          res = JSON.stringify(res);
-          console.log(res);
-          channel.sendToQueue(msg.properties.replyTo, Buffer.from(res), {correlationId: msg.properties.correlationId});
-          channel.ack(msg)
-        });
+            res = JSON.stringify(res);
+            console.log(res);
+            channel.sendToQueue(msg.properties.replyTo, Buffer.from(res), {correlationId: msg.properties.correlationId});
+            channel.ack(msg)
+          });
       });
     });
   });
@@ -106,14 +155,22 @@ function add_item(payload, callback){
   });
 }
 
-//TODO del tweet
-function del_item(){}
+function del_item(payload, callback){
+  mongodb.remove("tweet", payload, function(err){
+    if(err) return callback(err);
+    return callback(null)
+  });
+}
 
 function search_item(id, options, callback){
   if(id === undefined){
     var query = {timestamp: {$lte: options.timestamp}};
-    if(options.username !== undefined) query['username'] = options.username
-    console.log(options.projections)
+    if(options.username !== undefined) query['username'] = options.username;
+    if(options.query !== undefined){
+      query['$text'] = {$search: options.query};
+    }
+    console.log(query)
+    console.log(options)
     mongodb.search("tweet", query, options.projections ,options.limit, {'timestamp': -1},function(err, result){
       if(err && err.message == "No matches") return callback(null, []);
       if(err) return callback(err, null);
