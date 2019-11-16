a44e8ab7a83c634e7f0481287b25a0735c8c6831
Weixin Tan <weixin.tan@stonybrook.edu>
2019-11-16 16:57:29 -0500

    Endpoint: Like A Tweet (#19)
    * changed status code for unauthorize from 500 to 401
    * limit greater than 100 will set to 100 and less than 1 will set to 1
     * added a like endpoint
    * tweet_management microservice handle like msg from like_item queue
    * mongodb utils added an update wrapper function

--- EntryPoint/src/request_checker.js
+++ EntryPoint/src/request_checker.js
@@ -9,7 +9,7 @@ module.exports = {
 
     verify : function(username, res){
         if(!username){
-            res.statusCode = 500;
+            res.statusCode = 401;
             res.json({"status":"error", "error":"Not log in"});
             return false;
         }
@@ -77,9 +77,10 @@ module.exports = {
     
     if(!Number.isFinite(ts) || !Number.isInteger(limit))
       return {"status":"error" , "error":"Either ts or limit is not type int."};
-    else if(limit < 1 || limit > 100)
-      return {"status":"error", "error":"limit is either less than 1 or greater than 100."}
-    
+    else if(limit < 1)
+      limit = 25;
+    else if(limit > 100)
+      limit = 100;
     if(login_username != null){
       if(following)
         msg_json["login_username"] = login_username;
