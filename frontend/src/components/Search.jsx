7e8fff220613321f4bdfdf91c3a4237c079c34c1
tanweixin98 <weixin.tan@stonybrook.edu>
2019-11-17 21:56:31 +0000

    merge issue and build dependency

---
+++ frontend/src/components/Search.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Form, Container, Button, Row } from 'react-bootstrap'
+import Tweet from './Tweet'
+
+class Search extends Component {
+    state = {
+        posts: null
+    }
+
+    static getDerivedStateFromProps(nextProps) {
+        if (nextProps.location.state)
+            return { posts: nextProps.location.state.posts }
+        else
+            return null
+    }
+
+    render = () => {
+        var searchResults = this.state.posts ? this.state.posts.map(post => { 
+                return <Tweet  username = {post.username}
+                            post_id = {post.id}
+                            content = {post.content}
+                            time = {post.timestamp}
+                            likes = {post.likes}/>}) : undefined
+        return (
+            <div>
+                <h1>Search results</h1>
+                {searchResults}
+            </div>
+        )
+    }
+}
+
+export default Search
